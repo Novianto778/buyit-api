951b202248f4fec6bec51a8f41ea5c5d5089de35
novianto22 <99942803+novianto22@users.noreply.github.com>
2022-12-21 18:25:07 +0700

    Change allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
     'http://127.0.0.1:5173',
-    'https://buyit-fe.vercel.app/',
+    'https://buyit-fe.vercel.app',
 ];
 
 module.exports = allowedOrigins;
