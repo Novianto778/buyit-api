2973cce62bb87ca8519eb56e3996273f5dae01fb
Novianto778 <63406837+Novianto778@users.noreply.github.com>
2023-05-21 22:04:50 +0700

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'https://buyit-fe.vercel.app',
-    
+    'https://buyit.up.railway.app/'
 ];
 
 module.exports = allowedOrigins;
